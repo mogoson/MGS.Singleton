698390a84ddb0ad19f19a98df3ca461372de7123
Mogoson <mogoson@outlook.com>
2025-10-09 20:57:56 +0800

    Optimize

--- Assets/Runtime/Agent/RoutineAgent.cs
+++ Assets/Runtime/Agent/RoutineAgent.cs
@@ -1,7 +1,7 @@
 /*************************************************************************
  *  Copyright © 2025 Mogoson All rights reserved.
  *------------------------------------------------------------------------
- *  File         :  CoroutineAgent.cs
+ *  File         :  RoutineAgent.cs
  *  Description  :  Default.
  *------------------------------------------------------------------------
  *  Author       :  Mogoson
@@ -11,75 +11,86 @@
  *************************************************************************/
 
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace MGS.Singleton
 {
-    /// <summary>
-    /// Agent of coroutine.
-    /// Do not add this component to any gameobject by yourself.
-    /// Require the first access is from the main thread.
-    /// </summary>
-    public sealed class CoroutineAgent : MonoSingleton<CoroutineAgent>
+    public sealed class RoutineAgent
     {
         /// <summary>
-        /// Start coroutine to invoke the specified action repeatedly each frame.
+        /// Routine to invoke the specified action repeatedly each frame.
         /// </summary>
         /// <param name="tick">The action to invoke.</param>
         /// <returns>The coroutine object.</returns>
-        public Coroutine StartTickCoroutine(Action tick)
+        public static IEnumerator TickRoutine(Action tick)
         {
-            var routine = RoutineAgent.TickRoutine(tick);
-            return StartCoroutine(routine);
+            while (true)
+            {
+                tick?.Invoke();
+                yield return null;
+            }
         }
 
         /// <summary>
-        /// Start coroutine to invoke the specified action repeatedly each frame with a specified time interval.
+        /// Routine to invoke the specified action repeatedly each frame with a specified time interval.
         /// </summary>
         /// <param name="seconds">The time interval between invocations.</param>
         /// <param name="tick">The action to invoke.</param>
         /// <param name="arrive">The action to invoke when the timer arrives.</param>
         /// <returns>The coroutine object.</returns>
-        public Coroutine StartTimerCoroutine(float seconds, Action<float> tick, Action arrive)
+        public static IEnumerator TimerRoutine(float seconds, Action<float> tick, Action arrive)
         {
-            var routine = RoutineAgent.TimerRoutine(seconds, tick, arrive);
-            return StartCoroutine(routine);
+            var timer = 0f;
+            while (timer < seconds)
+            {
+                timer += Time.deltaTime;
+                tick?.Invoke(timer);
+                yield return null;
+            }
+            arrive?.Invoke();
         }
 
         /// <summary>
-        /// Start coroutine to invoke the specified action when the specified condition is true.
+        /// Routine to invoke the specified action when the specified condition is true.
         /// </summary>
         /// <param name="condition">The condition to check.</param>
         /// <param name="action">The action to invoke.</param>
         /// <returns>The coroutine object.</returns>
-        public Coroutine StartWaitCoroutine(Func<bool> condition, Action action)
+        public static IEnumerator WaitRoutine(Func<bool> condition, Action action)
         {
-            var routine = RoutineAgent.WaitRoutine(condition, action);
-            return StartCoroutine(routine);
+            while (!condition.Invoke())
+            {
+                yield return null;
+            }
+            action?.Invoke();
         }
 
         /// <summary>
-        /// Start coroutine to invoke the specified action repeatedly each frame until the specified condition is false.
+        /// Routine to invoke the specified action repeatedly each frame until the specified condition is false.
         /// </summary>
         /// <param name="condition">The condition to check.</param>
         /// <param name="action">The action to invoke.</param>
         /// <returns>The coroutine object.</returns>
-        public Coroutine StartUntilCoroutine(Func<bool> condition, Action action)
+        public static IEnumerator UntilRoutine(Func<bool> condition, Action action)
         {
-            var routine = RoutineAgent.UntilRoutine(condition, action);
-            return StartCoroutine(routine);
+            while (condition.Invoke())
+            {
+                action?.Invoke();
+                yield return null;
+            }
         }
 
         /// <summary>
-        /// Start coroutine to invoke the specified action after a specified delay seconds.
+        /// Routine to invoke the specified action after a specified delay seconds.
         /// </summary>
         /// <param name="seconds">The delay in seconds.</param>
         /// <param name="action">The action to invoke.</param>
         /// <returns>The coroutine object.</returns>
-        public Coroutine StartDelayCoroutine(float seconds, Action action)
+        public static IEnumerator DelayRoutine(float seconds, Action action)
         {
-            var routine = RoutineAgent.DelayRoutine(seconds, action);
-            return StartCoroutine(routine);
+            yield return new WaitForSeconds(seconds);
+            action?.Invoke();
         }
     }
 }
